7f07b437ffab730227d9062c9ecbc6608b73a99a
Jorge Zuluaga <zuluagajorge@gmail.com>
2014-12-17 16:47:13 -0500

    Commit

--- BHMfigures.py
+++ BHMfigures.py
@@ -545,7 +545,6 @@ def IntegratedReferenceValues():
     #########################################
     
 
-
 #CompareLuminositiesMassLoss()
 #IntegratedReferenceFluxes()
 IntegratedReferenceValues()
