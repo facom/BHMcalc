28e565b4132a6799e3995522b3c0feda7a844f9f
Jorge Zuluaga <zuluagajorge@gmail.com>
2014-12-27 11:32:02 -0500

    Commit

---
+++ web/about.php
@@ -0,0 +1,64 @@
+<?PHP
+$VERSION=rtrim(shell_exec("make version"));
+$about=<<<C
+<div class="tabbertab" id="Introduction" title="About">
+  <div class="tabcontent">
+    <p>
+      $BHMcalc is the result of the fruitful collaboration between the
+      group of
+      <b>Prof. Paul Mason</b> in
+      the <a href="http://academics.utep.edu/Default.aspx?tabid=18742">University
+      of Texas, El Paso</a> and the group of <b>Prof. Jorge
+      I. Zuluaga</b>,
+      the <a href="http://urania.udea.edu.co/facom">Computational
+      Physics and Astrophysics Group</a> of the University of
+      Antioquia (Medell&iacute;n, Colombia).
+    </p>
+  
+    <a name="References"></a>
+    <p>
+      To cite $BHMcalc please refer to one or all of the following
+      papers:
+
+      <ul>
+	<li>
+	  <a href="http://adsabs.harvard.edu/abs/2013ApJ...774L..26M">[Zuluaga+2015]</a>
+	  Zuluaga, J.I., Mason, P.A. & Cuartas-Restrepo,
+	  P. A. (2015). <i>Constraining the Radiation and Plasma
+	  Environment of Kepler Circumbinary habitable zone
+	  planets</i>. Submitted to the Astrophysical Journal. ArXiv
+	  e-prints (ADS:
+	  2013ApJ...774L..26M, <a href="http://arxiv.org/abs/1307.4624">arXiv:1412.4624</a>)
+	</li>
+	<br/>
+	<li>
+	  <a href="http://adsabs.harvard.edu/abs/2013ApJ...774L..26M">[Mason+2013]</a>
+	  Mason, P. A., Zuluaga, J. I., Clark, J. M., &
+	  Cuartas-Restrepo, P. A. (2013). <i>Rotational Synchronization
+	  May Enhance Habitability for Circumbinary Planets: Kepler
+	  Binary Case Studies</i>. <b>The Astrophysical Journal Letters</b>,
+	  774(2), L26. (ADS:
+	  2013ApJ...774L..26M, <a href="http://arxiv.org/abs/1307.4624">arXiv:1307.4624</a>)
+	</li>
+	<br/>
+	<li>
+	  <a href="http://adsabs.harvard.edu/abs/2014arXiv1408.5163M">[Mason+2014]</a>
+	  Mason, P. A., Zuluaga, J. I., Cuartas-Restrepo, P. A., &
+	  Clark, J. M. (2014). Circumbinary Habitability
+	  Niches. <b>International Journal of Astrobiology</b>.  (ADS:
+	  2014arXiv1408.5163M, <a href="http://arxiv.org/abs/1408.5163">arXiv:1408.5163</a>)
+	</li>
+      </ul>	  
+    </p>
+    
+    <p>
+      This is the second version of the $BHMcalc.  A previous and
+      simpler version of the tool was released along
+      the <b>[Mason+2014]</b> paper and it is presently unavailable.
+    </p>
+
+    <pre>$VERSION</pre>
+  </div>
+</div>
+C;
+?>
