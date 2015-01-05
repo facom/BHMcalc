aba9d91df5bf7be4a050ccb3c21c3a94f3e17379
Jorge Zuluaga <zuluagajorge@gmail.com>
2015-01-04 23:45:56 -0500

    Commit

--- index.php
+++ index.php
@@ -1885,13 +1885,13 @@ $serverip=$_SERVER["SERVER_ADDR"];
 if($serverip!="127.0.0.1"){
 $CONTENT.=<<<TRACKING
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 							       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-														  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+														  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 
-ga('create', 'UA-58223004-1', 'auto');
-ga('send', 'pageview');
+ga("create", "UA-58223004-1", "auto");
+ga("send", "pageview");
 </script>
 TRACKING;
 }
