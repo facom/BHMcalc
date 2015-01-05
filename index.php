48319a9774d7abdbc3de82521b552631d74d8c34
Jorge Zuluaga <zuluagajorge@gmail.com>
2015-01-05 13:53:51 -0500

    Commit

--- index.php
+++ index.php
@@ -1819,7 +1819,7 @@ $header
 <body>
     <div id="body" style="background-color:white;opacity:0.2;width:100%;height:100%">
     <center>
-      <div id="rendering" style="z-index:10000;float:center;position:absolute;right:0px;border:solid black 1px;">
+      <div id="rendering" style="z-index:10000;float:center;position:absolute;right:0px;border:solid black 0px;">
 	<img src="web/load.gif">
       </div>
     </center>
